880c51dcd15f5343b5933234e1cc6ce50992ddc6
Austin Miller <ajm19x@my.fsu.edu>
2021-08-08 16:32:19 -0600

    Initial Commit

---
+++ Weather App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather App
+//
+//  Created by austin miller on 8/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
