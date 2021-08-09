4930f6c5dd722aa51af75cd551b31f7b0701f46c
Austin Miller <ajm19x@my.fsu.edu>
2021-08-09 14:09:32 -0600

    Adds background functionality to app

--- Weather App/WeatherView.swift
+++ Weather App/WeatherView.swift
@@ -6,24 +6,34 @@
 //
 
 import SwiftUI
-
+extension Color{
+    static let BackgroundGray = Color("BackGroundGrey")
+    static let BackgroundBlue = Color("BackgroundBlue")
+}
 struct WeatherView: View {
     
     @ObservedObject var viewModel: WeatherViewModel
     
     var body: some View {
-        VStack{
-            Text(viewModel.cityName)
-                .font(.largeTitle)
-                .padding()
-            Text(viewModel.temperature)
-                .font(.system(size: 70))
-                .bold()
-            Text(viewModel.weatherIcon)
-                .font(.largeTitle)
-                .padding()
-            Text(viewModel.weatherDescription)
-        }.onAppear(perform: viewModel.refresh)
+        ZStack{
+            if(viewModel.weatherIcon == "☀️" ){
+                Color.BackgroundBlue.edgesIgnoringSafeArea(.all)
+                
+            }
+            else {Color.BackgroundGray.edgesIgnoringSafeArea(.all)}
+            VStack{
+                Text(viewModel.weatherIcon)
+                    .font(.system(size: 80))
+                    .padding()
+                Text(viewModel.cityName)
+                    .font(.largeTitle)
+                    .padding()
+                Text(viewModel.temperature)
+                    .font(.system(size: 70))
+                    .bold()
+                Text(viewModel.weatherDescription)
+            }.onAppear(perform: viewModel.refresh)
+        }
     }
 }
 
